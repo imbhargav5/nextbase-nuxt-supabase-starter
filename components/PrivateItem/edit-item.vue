6390714c5dd8c981f0930adc687ab43e84be19a5
Mohini Mahato <mohinimahatowork@gmail.com>
2024-05-23 10:19:29 +0530

    db.json removed which I created to fetch data

--- components/PrivateItem/edit-item.vue
+++ components/PrivateItem/edit-item.vue
@@ -1,76 +1,57 @@
-<!-- EditItem.vue -->
+<!-- edit-item.vue -->
 <script setup lang="ts">
-import { ref, watch, defineProps, defineEmits } from 'vue';
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { ref, watch } from 'vue';
 
 const props = defineProps<{
-  visible: boolean;
-  item: {
-    id: string | number;
-    title: string;
-    description: string;
-    date: string;
-  };
+  item: { id: string | number; title: string; description: string; date: string };
 }>();
 
-const emit = defineEmits(['updateItem', 'update:visible']);
+const emit = defineEmits(['updateItem', 'close']);
 
-const formData = ref({ ...props.item });
+const itemName = ref(props.item.title);
+const itemDescription = ref(props.item.description);
 
 watch(() => props.item, (newItem) => {
-  formData.value = { ...newItem };
+  itemName.value = newItem.title;
+  itemDescription.value = newItem.description;
 });
 
-const handleSubmit = () => {
-  emit('updateItem', formData.value);
-  closeModal();
+const saveItem = () => {
+  const updatedItem = {
+    ...props.item,
+    title: itemName.value,
+    description: itemDescription.value,
+  };
+
+  emit('updateItem', updatedItem);
 };
 
 const closeModal = () => {
-  emit('update:visible', false);
+  emit('close');
 };
 </script>
 
 <template>
-  <Dialog :open="props.visible" @close="closeModal">
+  <Dialog :open="true" @close="closeModal">
     <DialogContent class="sm:max-w-[425px]">
       <DialogHeader>
         <DialogTitle class="text-lg font-inter">Edit private item</DialogTitle>
         <DialogDescription class="text-sm text-[#64748B] font-inter">
-          Update the details of your private item.
+          This item will be private and only you logged in will be able to edit it.
         </DialogDescription>
       </DialogHeader>
       <div class="grid gap-4 py-1">
         <div class="gap-4">
-          <Label for="item-title" class="text-sm text-right font-inter">
-            Title
-          </Label>
-          <Input v-model="formData.title" id="item-title" type="text" placeholder="Type your item title" class="col-span-3 mt-3 font-inter" />
+          <Label for="item-name" class="text-sm text-right font-inter">Name</Label>
+          <Input v-model="itemName" id="item-name" type="text" placeholder="Type your item name" class="col-span-3 mt-3 font-inter" />
         </div>
         <div class="gap-4">
-          <Label for="item-description" class="text-sm text-right font-inter">
-            Description
-          </Label>
-          <textarea v-model="formData.description" id="item-description" placeholder="Enter your description" class="col-span-3 mt-3 font-inter"></textarea>
+          <Label for="item-description" class="text-sm text-right font-inter">Description</Label>
+          <Textarea v-model="itemDescription" id="item-description" placeholder="Enter your description" class="col-span-3 mt-3 font-inter" />
         </div>
       </div>
       <DialogFooter>
-        <Button type="button" class="bg-[#0F172A] w-[100%] font-inter font-semibold" @click="handleSubmit">
-          Save Changes
-        </Button>
-        <Button type="button" class="w-[100%] font-inter font-semibold" @click="closeModal">
-          Cancel
-        </Button>
+        <Button type="submit" class="bg-[#0F172A] w-[100%] font-inter font-semibold" @click="saveItem">Update Item</Button>
       </DialogFooter>
     </DialogContent>
   </Dialog>
